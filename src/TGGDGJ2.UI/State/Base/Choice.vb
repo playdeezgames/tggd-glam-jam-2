ccb7bb837faeb9ff6062b0d27410546c0c55e3ea
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-01-01 05:24:49 -0600

    i have a main menu

---
+++ src/TGGDGJ2.UI/State/Base/Choice.vb
@@ -0,0 +1,10 @@
+﻿Imports TGGD.UI
+
+Friend Class Choice
+    Sub New(text As String, choose As Func(Of IUIState))
+        Me.Text = text
+        Me.Choose = choose
+    End Sub
+    ReadOnly Property Text As String
+    ReadOnly Property Choose As Func(Of IUIState)
+End Class
---
+++ src/TGGDGJ2.UI/State/Menu/MainMenuState.vb
@@ -0,0 +1,29 @@
+﻿Imports TGGD.UI
+
+Friend Class MainMenuState
+    Inherits PickerState
+    Implements IUIState
+
+    Public Sub New(
+                  buffer As IUIBuffer(Of Integer),
+                  world As Business.IWorld,
+                  doEvent As Action(Of String()))
+        MyBase.New(
+            buffer,
+            world,
+            doEvent,
+            0,
+            MakeChoices(buffer, world, doEvent))
+    End Sub
+
+    Private Shared Function MakeChoices(
+                                buffer As IUIBuffer(Of Integer),
+                                world As Business.IWorld,
+                                doEvent As Action(Of String())) As IEnumerable(Of Choice)
+        Return {
+            New Choice("Embark!", Function() Nothing),
+            New Choice("Story", Function() Nothing),
+            New Choice("About", Function() Nothing)
+            }
+    End Function
+End Class
