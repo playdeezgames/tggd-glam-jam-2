ccb70ab710f23ef50c74a3a004a0b7eca813af64
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-01-04 04:23:44 -0600

    mapping stuff

--- src/TGGDGJ2.Business/Grimoire/Grimoire.vb
+++ src/TGGDGJ2.Business/Grimoire/Grimoire.vb
@@ -1,4 +1,2 @@
 ﻿Friend Module Grimoire
-    Friend Const MAP_COLUMNS = 32
-    Friend Const MAP_ROWS = 16
 End Module
--- src/TGGDGJ2.Business/Map/MapType/StartingAreaMapType.vb
+++ src/TGGDGJ2.Business/Map/MapType/StartingAreaMapType.vb
@@ -1,32 +1,70 @@
-﻿Imports System.Data.Common
-
-Friend Class StartingAreaMapType
+﻿Friend Class StartingAreaMapType
     Inherits MapType
 
     Private Sub New()
-        MyBase.New(Name, MAP_COLUMNS, MAP_ROWS)
+        MyBase.New(Name, grid(0).Length, grid.Length)
+    End Sub
+
+    Private Shared ReadOnly grid As String() =
+        {
+            "################################",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#              @               +",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "#                              #",
+            "################################"
+        }
+
+    Private Shared ReadOnly locationTypeTable As IReadOnlyDictionary(Of Char, ILocationType) =
+        New Dictionary(Of Char, ILocationType) From
+        {
+            {" "c, FloorLocationType.Instance},
+            {"@"c, FloorLocationType.Instance},
+            {"#"c, WallLocationType.Instance},
+            {"+"c, LockedDoorLocationType.Instance}
+        }
+
+    Private Shared ReadOnly characterTypeTable As IReadOnlyDictionary(Of Char, ICharacterType) =
+        New Dictionary(Of Char, ICharacterType) From
+        {
+            {"@"c, N00bCharacterType.Instance}
+        }
+
+    Private Shared ReadOnly postProcessing As IReadOnlyDictionary(Of Char, Action(Of ILocation)) =
+        New Dictionary(Of Char, Action(Of ILocation)) From
+        {
+            {"@"c, AddressOf SetAvatarCharacter}
+        }
+
+    Private Shared Sub SetAvatarCharacter(location As ILocation)
+        location.World.Avatar = location.Character
     End Sub
 
     Public Overrides Sub Initialize(map As IMap)
-        For Each column In Enumerable.Range(0, map.Columns)
-            map.CreateLocation(column, 0, WallLocationType.Instance)
-            map.CreateLocation(column, map.Rows - 1, WallLocationType.Instance)
-        Next
-        For Each row In Enumerable.Range(1, map.Rows - 2)
-            map.CreateLocation(0, row, WallLocationType.Instance)
-            If row = map.Rows \ 2 Then
-                map.CreateLocation(map.Columns - 1, row, LockedDoorLocationType.Instance)
-            Else
-                map.CreateLocation(map.Columns - 1, row, WallLocationType.Instance)
-            End If
-        Next
-        For Each column In Enumerable.Range(1, map.Columns - 2)
-            For Each row In Enumerable.Range(1, map.Rows - 2)
-                map.CreateLocation(column, row, FloorLocationType.Instance)
+        For Each row In Enumerable.Range(0, grid.Length)
+            For Each column In Enumerable.Range(0, grid(row).Length)
+                Dim gridCell = grid(row)(column)
+                Dim locationType = locationTypeTable(gridCell)
+                Dim location = map.CreateLocation(column, row, locationType)
+                Dim characterType As ICharacterType = Nothing
+                If characterTypeTable.TryGetValue(gridCell, characterType) Then
+                    location.CreateCharacter(characterType)
+                End If
+                Dim postProcessor As Action(Of ILocation) = Nothing
+                postProcessing.TryGetValue(gridCell, postProcessor)
+                postProcessor?.Invoke(location)
             Next
         Next
-        Dim location = map.GetLocation(map.Columns \ 2, map.Rows \ 2)
-        map.World.Avatar = location.CreateCharacter(N00bCharacterType.Instance)
     End Sub
 
     Friend Shared ReadOnly Name As String = NameOf(StartingAreaMapType)
