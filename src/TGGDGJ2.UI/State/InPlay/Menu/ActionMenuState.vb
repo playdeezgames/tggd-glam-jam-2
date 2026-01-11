7ec17474894d2c056fc8ec6f362f8958ad62ee7d
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-01-11 05:48:30 -0600

    stubbed in the status screen

--- src/TGGDGJ2.UI/State/InPlay/Menu/ActionMenuState.vb
+++ src/TGGDGJ2.UI/State/InPlay/Menu/ActionMenuState.vb
@@ -16,7 +16,8 @@ Friend Class ActionMenuState
 
     Private Shared Function GenerateChoices(buffer As IUIBuffer(Of Integer), world As Business.IWorld, doEvent As Action(Of String())) As IEnumerable(Of Choice)
         Dim result As New List(Of Choice) From {
-            New Choice("Never Mind", Function() InPlayState.Launch(buffer, world, doEvent))
+            New Choice("Never Mind", Function() InPlayState.Launch(buffer, world, doEvent)),
+            New Choice("Status", Function() StatusState.Launch(buffer, world, doEvent))
         }
         If world.Avatar.HasItems Then
             result.Add(New Choice("Inventory", Function() InventoryMenuState.Launch(buffer, world, doEvent)))
---
+++ src/TGGDGJ2.UI/State/InPlay/Menu/StatusState.vb
@@ -0,0 +1,22 @@
+﻿Imports TGGD.UI
+
+Friend Class StatusState
+    Inherits BaseState
+
+    Private Sub New(buffer As IUIBuffer(Of Integer), world As Business.IWorld, doEvent As Action(Of String()))
+        MyBase.New(buffer, world, doEvent)
+    End Sub
+
+    Public Overrides Sub Refresh()
+        buffer.Fill(32)
+        buffer.WriteText((0, 0), "TODO: Status", 0)
+    End Sub
+
+    Friend Shared Function Launch(buffer As IUIBuffer(Of Integer), world As Business.IWorld, doEvent As Action(Of String())) As IUIState
+        Return New StatusState(buffer, world, doEvent)
+    End Function
+
+    Public Overrides Function HandleCommand(command As String) As IUIState
+        Return New ActionMenuState(buffer, world, doEvent)
+    End Function
+End Class
