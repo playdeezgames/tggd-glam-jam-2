eb877ba8ca3e14afac3405a860530d8ef4284450
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-01-11 06:02:08 -0600

    status screen

--- src/TGGDGJ2.UI/State/InPlay/Menu/StatusState.vb
+++ src/TGGDGJ2.UI/State/InPlay/Menu/StatusState.vb
@@ -1,15 +1,31 @@
 ﻿Imports TGGD.UI
+Imports TGGDGJ2.Business
 
 Friend Class StatusState
     Inherits BaseState
 
     Private Sub New(buffer As IUIBuffer(Of Integer), world As Business.IWorld, doEvent As Action(Of String()))
         MyBase.New(buffer, world, doEvent)
+        frame.Box(0, 0, buffer.Columns, 3, True)
     End Sub
 
     Public Overrides Sub Refresh()
         buffer.Fill(32)
-        buffer.WriteText((0, 0), "TODO: Status", 0)
+        buffer.WriteCenteredText(1, "Status", 0)
+        Dim position = (0, 3)
+        Dim health = world.Avatar.GetCounter(Counters.Health)
+        Dim satiety = world.Avatar.GetCounter(Counters.Satiety)
+        Dim attackValue = world.Avatar.GetAttackValue()
+        Dim defendValue = world.Avatar.GetDefendValue()
+        Dim xp = world.Avatar.GetCounter(Counters.XP)
+        Dim xpLevel = world.Avatar.GetCounter(Counters.XPLevel).Value
+        position = buffer.WriteLine(position, $"Health: {health.Value}/{health.Maximum}", 0)
+        position = buffer.WriteLine(position, $"Satiety: {Satiety.Value}/{Satiety.Maximum}", 0)
+        position = buffer.WriteLine(position, $"Attack: {attackValue}", 0)
+        position = buffer.WriteLine(position, $"Defend: {defendValue}", 0)
+        position = buffer.WriteLine(position, $"XP: {xp.Value}/{xp.Maximum}", 0)
+        position = buffer.WriteLine(position, $"XP Level: {xpLevel}", 0)
+        buffer.DrawFrame((0, 0), frame)
     End Sub
 
     Friend Shared Function Launch(buffer As IUIBuffer(Of Integer), world As Business.IWorld, doEvent As Action(Of String())) As IUIState
